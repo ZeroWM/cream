6f8b91ec735b974713d97dc6800d75235e24b845
mei.wang01 <mei.wang01@mljr.com>
2018-12-15 17:43:44 +0800

    提交jdbc demo

--- src/main/java/jdbcDemo/JdbcFirstDemo.java
+++ src/main/java/jdbcDemo/JdbcFirstDemo.java
@@ -9,8 +9,26 @@ import java.sql.*;
  * @create: 2018-12-15 16:21
  **/
 public class JdbcFirstDemo {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
+        final  String user = "root";
+        final  String password = "123456";
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cream?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&useCursorFetch=true&useCursorFetch=true&allowMultiQueries=true",user,password);
+
+        Statement statement = conn.createStatement();
+        String sql = "select * from user ";
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            Long uuid =  resultSet.getLong("uuid");
+            String user_name = resultSet.getString("user_name");
+            System.out.println("---------" + uuid);
+            System.out.println("---------" + user_name);
+        }
+
+        resultSet.close();
+        statement.close();
+        conn.close();
 
     }
 }
