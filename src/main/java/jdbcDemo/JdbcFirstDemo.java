0612bdfa5ebf1cefff9b4a9bcb81a8f55ebd85fe
mei.wang01 <mei.wang01@mljr.com>
2018-12-15 16:25:52 +0800

    提交信息

---
+++ src/main/java/jdbcDemo/JdbcFirstDemo.java
@@ -0,0 +1,16 @@
+package jdbcDemo;/**
+ * Created by wm on 2018/12/15.
+ */
+import java.sql.*;
+/**
+ * @program: cream
+ * @description: JDBC第一个项目实例
+ * @author: WangMei
+ * @create: 2018-12-15 16:21
+ **/
+public class JdbcFirstDemo {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+    }
+}
