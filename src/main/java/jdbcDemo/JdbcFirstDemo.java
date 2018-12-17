9d4e092988dbeb6c7aea2faa7506ed65bedcc6e4
mei.wang01 <mei.wang01@mljr.com>
2018-12-17 14:39:37 +0800

    添加jdbc注释

--- src/main/java/jdbcDemo/JdbcFirstDemo.java
+++ src/main/java/jdbcDemo/JdbcFirstDemo.java
@@ -10,15 +10,23 @@ import java.sql.*;
  **/
 public class JdbcFirstDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        //注册jdbc驱动
         Class.forName("com.mysql.jdbc.Driver");
         final  String user = "root";
         final  String password = "123456";
+
+        //通过驱动管理创建连接
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cream?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&useCursorFetch=true&useCursorFetch=true&allowMultiQueries=true",user,password);
 
+        //创建执行sql
         Statement statement = conn.createStatement();
         String sql = "select * from user ";
+
+        //返回结果集
         ResultSet resultSet = statement.executeQuery(sql);
 
+        //结果集，
         while (resultSet.next()) {
             Long uuid =  resultSet.getLong("uuid");
             String user_name = resultSet.getString("user_name");
