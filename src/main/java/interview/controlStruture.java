9459a9ade602657c75b95e192e1c597a838752ec
mei.wang01 <mei.wang01@mljr.com>
2019-05-07 15:29:22 +0800

    Algorithm

---
+++ src/main/java/interview/controlStruture.java
@@ -0,0 +1,29 @@
+package interview; /**
+ * Created by wm on 2019/5/7.
+ */
+
+/**
+ * @program: switch的坑
+ * @description: interview.controlStruture
+ * @author: WangMei
+ * @create: 2019-05-07 11:31
+ **/
+public class controlStruture {
+
+    public  static  int testSwitch(int i ){
+        int result = 0;
+        switch (i){
+            case 1:
+                result = result + i;
+            case 2:
+                result = result + i*2;
+            case 3:
+                result = result + i*3;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(testSwitch(1));
+    }
+}
