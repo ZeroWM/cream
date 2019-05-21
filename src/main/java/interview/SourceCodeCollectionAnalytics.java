801fe0de0df922b1c6583818edcfd6c597c3e3ad
mei.wang01 <mei.wang01@mljr.com>
2019-05-21 15:00:26 +0800

    Algorithm InsertSort

--- src/main/java/interview/SourceCodeCollectionAnalytics.java
+++ src/main/java/interview/SourceCodeCollectionAnalytics.java
@@ -2,8 +2,7 @@ package interview;/**
  * Created by wm on 2019/5/12.
  */
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -13,7 +12,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author: WangMei
  * @create: 2019-05-12 11:24
  **/
-public  class SourceCodeAnalytics {
+public  class SourceCodeCollectionAnalytics {
     public static void main(String[] args) {
         AtomicInteger a = new AtomicInteger();
         HashMap m = new HashMap();
@@ -21,13 +20,20 @@ public  class SourceCodeAnalytics {
         ArrayList arrayList = new ArrayList();
         arrayList.contains("");
         String s = "hello";
-        SourceCodeAnalytics t = new SourceCodeAnalytics();
+        SourceCodeCollectionAnalytics t = new SourceCodeCollectionAnalytics();
         t.func1(s);
         System.out.println("----------s:" + s);
         StringBuilder s2 = new StringBuilder("hello");
         t.func(s2);
         System.out.println("-----------s2:" + s2);
 
+
+        //map常见的实现类：
+        // Hashtable
+        // HashMap, ConcurrentHashMap, LinkedHashMap, WeakHashMap
+        // EnumMap
+        // Properties
+        // TreeMap
 
         HashMap<String, Integer> map = new HashMap<String, Integer>();
 //
@@ -65,6 +71,50 @@ public  class SourceCodeAnalytics {
         map1.get("1");
         map1.size();
         map1.containsKey("");
+
+        LinkedHashMap<String,String> linkedHashMap = new LinkedHashMap<String, String>();
+        linkedHashMap.put("语文", "1");
+        linkedHashMap.put("数学", "2");
+        linkedHashMap.put("英语", "3");
+
+        linkedHashMap.get("语文");
+        linkedHashMap.size();
+
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("wm");
+        list1.add("I");
+        list1.add("love");
+        list1.add("u");
+        list1.add("wm");
+        list1.add("I");
+        list1.add("love");
+        list1.add("u");
+        list1.add("wm");
+        list1.add("I");
+        list1.add("love");
+        list1.add("u");
+        list1.get(10);
+        list1.size();
+
+
+        ArrayList<String> list2 = new ArrayList<String>();
+        list2.add("wm");
+        list2.add("I");
+        list2.add("love");
+        list2.add("u");
+        list2.add("2");
+        list2.retainAll(list1);
+        list2.removeAll(list1);
+
+        LinkedList<String> linkedList = new LinkedList<String>();
+        linkedList.add("wm");
+        linkedList.add("I");
+        linkedList.add("love");
+        linkedList.get(10);
+        linkedList.getFirst();
+
+
+
 
 
     }
---
+++ src/main/java/interview/SourceCodeThreadAnalytics.java
@@ -0,0 +1,13 @@
+package interview;/**
+ * Created by wm on 2019/5/21.
+ */
+
+/**
+ * @program: cream
+ * @description: 线程happy
+ * @author: WangMei
+ * @create: 2019-05-21 14:59
+ **/
+public class SourceCodeThreadAnalytics {
+
+}
